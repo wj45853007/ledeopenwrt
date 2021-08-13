843c09f5ec3296c9a389473198da9aa24f2aacbd
wj45853007 <56184685+wj45853007@users.noreply.github.com>
2021-08-13 10:20:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,3 @@
 # Add a feed source
  sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
  sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
- sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
